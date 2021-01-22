e3283d10de8ef0fc8355cb62cf960e78ea20348f
Esthon Jr <esthon@gmail.com>
2021-01-22 15:37:13 -0300

    1st commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const greader = require("g-sheets-api");
+
+const readerOptions = {
+    sheetId: "1XV4tkoXIjKXzNJlWxDGp7vSeKKC40JpIfeeEodkXps8",
+    returnAllResults: true,
+    // filter: {
+    //   "key to filter on": "value to match",
+    // },
+  };
+
+const app = express();
+
+const port = 3000;
+ 
+app.get('/', function (req, res) {
+    greader(readerOptions, (results) => {
+        console.log(results);
+        res.send(results);
+    });
+    //res.send('Hello');
+});
+ 
+app.listen(port);
