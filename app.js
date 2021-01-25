3e1111a78d5c1cd6552e9e80c879b1ab23fad9c6
Esthon Jr <esthon@gmail.com>
2021-01-25 00:52:39 -0300

    etherscan

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 
@@ -9,16 +11,76 @@ app.use(cors());
 const port = process.env['PORT'] || 3000;
  
 app.get('/', function (req, res) {
-    axios.get('https://api.coingecko.com/api/v3/coins/yield-app?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false%27')
-    .then(function (response) {
-        // handle success
-        res.send((response.data.market_data.circulating_supply).toString());
-    })
+    // Contract Address- Market cap valuation
+    const cont1 = '0xf94b5c5651c888d928439ab6514b93944eee6f48';
+    // Company reserves not distributed 
+    const cont2 = "0x3cbCaD74225dd6a062008D3ed85Ee0339a7E2B2e";
+    // Company reserves not distributed 
+    const cont3 = "0x6D2FCeb4FEa5d959d3e3865727110122aFC15eA5"; 
+    // Team unlocked token not distributed
+    const cont4 = "0x13e3098BbeEE479B64760af8Ef9A968d36361783";
+
+    axios.all([
+        axios.get('https://api.etherscan.io/api', {
+            params: {
+                module: 'stats',
+                action: 'tokensupply',
+                contractaddress: cont1,
+                apikey: process.env['ETHERSCAN_API_TOKEN']
+
+            }
+        }),
+        axios.get('https://api.etherscan.io/api', {
+            params: {
+                module: 'account',
+                action: 'tokenbalance',
+                contractaddress: cont1,
+                address: cont2,
+                tag: 'latest',
+                apikey: process.env['ETHERSCAN_API_TOKEN']
+            }
+        }),
+        axios.get('https://api.etherscan.io/api', {
+            params: {
+                module: 'account',
+                action: 'tokenbalance',
+                contractaddress: cont1,
+                address: cont3,
+                tag: 'latest',
+                apikey: process.env['ETHERSCAN_API_TOKEN']
+            }
+        }),
+        axios.get('https://api.etherscan.io/api', {
+            params: {
+                module: 'account',
+                action: 'tokenbalance',
+                contractaddress: cont1,
+                address: cont4,
+                tag: 'latest',
+                apikey: process.env['ETHERSCAN_API_TOKEN']
+            }
+        })
+    ])
+    .then(axios.spread((data1, data2, data3, data4) => {
+        let result1 = data1.data.result;
+        let result2 = data2.data.result;
+        let result3 = data3.data.result;
+        let result4 = data4.data.result;
+
+        // pattern of 18 decimal places for the token
+        result1 = result1.slice(0,result1.length - 18) + "." + result1.slice(result1.length - 18, result1.length);
+        result2 = result2.slice(0,result2.length - 18) + "." + result2.slice(result2.length - 18, result2.length);
+        result3 = result3.slice(0,result3.length - 18) + "." + result3.slice(result3.length - 18, result3.length);
+        result4 = result4.slice(0,result4.length - 18) + "." + result4.slice(result4.length - 18, result4.length);
+
+        const circSupply = Number(result1) - (Number(result2) + Number(result3) + Number(result4));
+        res.send(circSupply.toLocaleString());
+    }))
     .catch(function (error) {
         // handle error
         console.log(error);
         res.send(401);
-  })
+    });
     //res.send('Hello');
 });
  
